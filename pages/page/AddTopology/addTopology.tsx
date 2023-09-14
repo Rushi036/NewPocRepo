6328b4e3bc4c3575faf2c3c523a39b4b16bc0d7f
Rushikesh Shiledar <rushikesh.shiledar@revdau.com>
2023-09-14 12:00:42 +0530

    css fixed of addtopo form with validation

--- pages/page/AddTopology/addTopology.tsx
+++ pages/page/AddTopology/addTopology.tsx
@@ -6,6 +6,7 @@ import React, { useState } from "react";
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 import Link from "next/link";
 import { Checkbox, FormControlLabel, FormGroup } from "@mui/material";
+import { title } from "process";
 const addTopology = () => {
   // eslint-disable-next-line react-hooks/rules-of-hooks
   const [rfInstance, setRfInstance] = useState<any>(null);
@@ -41,53 +42,76 @@ const addTopology = () => {
     e.preventDefault();
     // alert(process.env.NODE_ENV === 'development' ? process.env.REACT_APP_JSON_SERVER:'hi')
     async function dataFetch() {
-      let flow: any;
-      if (rfInstance) {
-        flow = rfInstance.toObject();
-      }
-      let arr: any[] = [];
-      flow.nodes.map((data: any) => {
-        arr.push(data.data.Path.Name);
-        // console.log()
-      });
-      flow = JSON.stringify(flow);
-      let topologyData = {
-        bu_id: localStorage.getItem("bu_id"),
-        flowChart: flow,
-        node_details: arr,
-        title: topologyTitle,
-        cloud_server: selectedClouds,
-        business_sponser: businessSponser,
-        server_owner : serverOwner,
-        application_owner : applicationOwner,
-        resource_owner : resourceOwner,
-       };
+      if (
+        topologyTitle &&
+        businessSponser &&
+        selectedClouds &&
+        serverOwner &&
+        applicationOwner &&
+        resourceOwner &&
+        selectedEnvironment
+      ) {
+        let flow: any;
+        if (rfInstance) {
+          flow = rfInstance.toObject();
+        }
+        let arr: any[] = [];
+        flow.nodes.map((data: any) => {
+          arr.push(data.data.Path.Name);
+          // console.log()
+        });
+        flow = JSON.stringify(flow);
+        let topologyData = {
+          bu_id: localStorage.getItem("bu_id"),
+          flowChart: flow,
+          node_details: arr,
+          title: topologyTitle,
+          cloud_server: selectedClouds,
+          business_sponser: businessSponser,
+          server_owner: serverOwner,
+          application_owner: applicationOwner,
+          resource_owner: resourceOwner,
+          selected_environment: selectedEnvironment,
+        };
 
-      console.log("topo data---------------",topologyData)
-      return await addTopologyData(topologyData);
+        console.log("topo data---------------", topologyData);
+        window.alert("Topology created successfully!");
+        return await addTopologyData(topologyData);
+      } else window.alert("Please fill out all the fields!");
     }
     // await dataFetch();
-    await dataFetch().then(async (res:any) => {
-      const date = new Date().toLocaleDateString("en-GB");
-      const topologyTableData = {
-        title: topologyTitle,
-        bu: localStorage.getItem("userName"),
-        bu_id: localStorage.getItem("bu_id"),
-        cloud_server: selectedClouds,
-        status: "Draft",
-        created_date: date,
-        topology_id: res.data[0].id,
-        business_sponser: businessSponser,
-        server_owner : serverOwner,
-        application_owner : applicationOwner,
-        resource_owner : resourceOwner,
-      };
-      console.log("topo data---------------",topologyTableData)
+    await dataFetch().then(async (res: any) => {
+      if (
+        topologyTitle &&
+        businessSponser &&
+        selectedClouds &&
+        serverOwner &&
+        applicationOwner &&
+        resourceOwner &&
+        selectedEnvironment
+      ) {
+        const date = new Date().toLocaleDateString("en-GB");
+        const topologyTableData = {
+          title: topologyTitle,
+          bu: localStorage.getItem("userName"),
+          bu_id: localStorage.getItem("bu_id"),
+          cloud_server: selectedClouds,
+          status: "Draft",
+          created_date: date,
+          topology_id: res.data[0].id,
+          business_sponser: businessSponser,
+          server_owner: serverOwner,
+          application_owner: applicationOwner,
+          resource_owner: resourceOwner,
+          selected_environment: selectedEnvironment,
+        };
+        console.log("topo data---------------", topologyTableData);
 
-      await addTableData(topologyTableData);
+        await addTableData(topologyTableData);
+      }
     });
   };
-  
+
   return (
     <>
       <form className="">
@@ -99,98 +123,116 @@ const addTopology = () => {
           <span className="ml-2">Add Topology</span>
         </div>
         <div className="mt-4 p-2 box-border w-full bg-white">
-          <div className=" px-2 space-y-3">
+          <div className=" pl-4 space-y-5 pt-4">
             <div className="flex justify-around">
-              <label htmlFor="" className="font-semibold text-base">
-                Topology Name :
+              <div className="flex w-2/6">
+                <label htmlFor="" className="font-semibold text-base w-36">
+                  Topology Name :
+                </label>
                 <input
                   type="text"
                   onChange={(e) => setTopologyTitle(e.target.value)}
                   required
-                  className="border-slate-600 border-2 rounded ml-2 w-[10rem]"
+                  className="border-slate-600 border-b-2 ml-2 rounded w-1/2"
                 />
-              </label>
-              <label htmlFor="" className="font-semibold text-base">
-                Environment :
+              </div>
+              <div className="flex w-2/6">
+                <label htmlFor="" className="font-semibold text-base w-28">
+                  Environment :
+                </label>
                 <select
                   onChange={handleEnvironmentChange}
                   value={selectedEnvironment}
-                  className="border-slate-600 border-2 rounded ml-2 w-[10rem] p-0.5"
+                  required
+                  className="border-slate-600 border-b-2 ml-2 rounded p-0.5 w-1/2"
                 >
                   <option value="" disabled>
-                    Select
+                    Select Environment
                   </option>
                   <option value="Production">Production</option>
                   <option value="Test">Test</option>
                   <option value="Development">Development</option>
                   <option value="UAT">UAT</option>
                 </select>
-              </label>
-              <label htmlFor="" className="font-semibold text-base">
-                Application Owner :
+              </div>
+              <div className="flex w-2/6">
+                <label htmlFor="" className="font-semibold text-base w-36">
+                  Application Owner :
+                </label>
                 <input
                   type="text"
                   onChange={(e) => setApplicationOwner(e.target.value)}
                   required
-                  className="border-slate-600 border-2 rounded ml-2 w-[10rem]"
+                  className="border-slate-600 border-b-2 rounded ml-2  w-1/2"
                 />
-              </label>
+              </div>
             </div>
             <div className="flex justify-around">
-              <label htmlFor="" className="font-semibold text-base">
-                Business Sponsor :
+              <div className="flex w-2/6">
+                <label htmlFor="" className="font-semibold text-base w-36">
+                  Business Sponsor :
+                </label>
                 <input
                   type="text"
                   onChange={(e) => setbusinessSponser(e.target.value)}
                   required
-                  className="border-slate-600 border-2 rounded ml-2 w-[10rem]"
+                  className="border-slate-600 border-b-2 rounded ml-2 w-1/2"
                 />
-              </label>
-              <label htmlFor="" className="font-semibold text-base">
-                Server Owner :
+              </div>
+              <div className="flex w-2/6">
+                <label htmlFor="" className="font-semibold text-base w-28">
+                  Server Owner :
+                </label>
                 <input
                   type="text"
                   onChange={(e) => setServerOwner(e.target.value)}
                   required
-                  className="border-slate-600 border-2 rounded ml-2 w-[10rem]"
+                  className="border-slate-600 border-b-2 ml-2 rounded w-1/2"
                 />
-              </label>
-              <label htmlFor="" className="font-semibold text-base">
-                Resource Owner :
+              </div>
+              <div className="flex w-2/6">
+                <label htmlFor="" className="font-semibold text-base w-36">
+                  Resource Owner :
+                </label>
                 <input
                   type="text"
                   onChange={(e) => setResourceOwner(e.target.value)}
                   required
-                  className="border-slate-600 border-2 rounded ml-2 w-[10rem]"
+                  className="border-slate-600 border-b-2 rounded ml-2 w-1/2"
                 />
-              </label>
+              </div>
             </div>
             <div className=" flex justify-center">
-              <label className="font-semibold text-base">Cloud Server :</label>
-              <div className="mx-3 flex space-x-4">
-                <div className="space-x-1">
-                  <input
-                    name="checkbox1"
-                    type="checkbox"
-                    value="AWS"
-                    onChange={handleCheckboxChange}
-                    checked={selectedClouds.includes("AWS")}
-                  />
-                  <label htmlFor="checkbox1 " className="font-semibold">
-                    AWS
-                  </label>
-                </div>
-                <div className="space-x-1">
-                  <input
-                    name="checkbox2"
-                    type="checkbox"
-                    value="Azure"
-                    onChange={handleCheckboxChange}
-                    checked={selectedClouds.includes("Azure")}
-                  />
-                  <label htmlFor="checkbox2" className="font-semibold">
-                    AZURE
-                  </label>
+              <div className="flex w-2/6">
+                <label className="font-semibold text-base w-28">
+                  Cloud Server :
+                </label>
+                <div className="mx-3 flex justify-around w-1/2 ">
+                  <div className="space-x-1">
+                    <input
+                      name="checkbox1"
+                      type="checkbox"
+                      value="AWS"
+                      required
+                      onChange={handleCheckboxChange}
+                      checked={selectedClouds.includes("AWS")}
+                    />
+                    <label htmlFor="checkbox1 " className="font-semibold">
+                      AWS
+                    </label>
+                  </div>
+                  <div className="space-x-1">
+                    <input
+                      name="checkbox2"
+                      type="checkbox"
+                      value="Azure"
+                      onChange={handleCheckboxChange}
+                      checked={selectedClouds.includes("Azure")}
+                    />
+                    <label htmlFor="checkbox2" className="font-semibold">
+                      AZURE
+                    </label>
+                  </div>
                 </div>
               </div>
             </div>
