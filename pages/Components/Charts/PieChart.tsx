d017b96a7a8bf9c9335587ab42b86afd3ccaa42e
Siddharth Nalwaya <siddharth.nalwaya@revdau.com>
2023-10-16 18:03:59 +0530

    changes

--- pages/Components/Charts/PieChart.tsx
+++ pages/Components/Charts/PieChart.tsx
@@ -5,73 +5,80 @@ import NoDataToDisplay from "highcharts/modules/no-data-to-display";
 
 // Initialize Highcharts modules
 
-const ChartComponent = (props:any) => {
+const ChartComponent = (props: any) => {
   useEffect(() => {
     HighchartsVariablePie(Highcharts);
     NoDataToDisplay(Highcharts);
-    Highcharts.chart("container-"+props.id, {
-      chart: {
-        type: "variablepie",
-      },
-      title: {
-        text: "Resource Group Distribution of Most Used Subscriptions",
-        align: "left",
-      },
-      tooltip: {
-        headerFormat: "",
-        pointFormat:
-          '<span style="color:{point.color}">\u25CF</span> <b> {point.name}</b><br/>' 
+  }, []);
+  useEffect(() => {
+    console.log(props.data.data);
+    if (props.data) {
+
+      const options: any = {
+        chart: {
+          plotBackgroundColor: null,
+          plotBorderWidth: null,
+          plotShadow: false,
+          type: 'pie',
+        },
+        title: {
+          text: props.data.title,
+          align: "left",
+        },
+        plotOptions: {
+          pie: {
+            size:'150px',
+            allowPointSelect: true,
+            cursor: 'pointer',
+            dataLabels: {
+              enabled: true,
+              format: '{point.percentage:.1f} %'
+            },
+            showInLegend: true
+          }
+        },
+        tooltip: {
+          headerFormat: "",
+          pointFormat:
+          '<span style="color:{point.color}">\u25CF</span> <b> {point.name}</b><br/>'
           +
-          "Area (square km): <b>{point.y}</b><br/>" +
-          "Population density (people per square km): <b>{point.z}</b><br/>",
-      },
-      lang: {
-        noData: "Please select atleast one Application",
-      },
-      noData: {
-        style: {
-          fontWeight: "bold",
-
-          fontSize: "15px",
-
-          color: "#303030",
+          `Total: <b>{point.y}</b> ${props.data.xAxis || ""}<br/>`,
         },
-      },
-      legend: {
-        enabled: true,
-      },
-      series: [
-        {
-          type: "variablepie",
-          minPointSize: 100,
-          innerSize: "40%",
-          zMin: 0,
-          name: "countries",
-          borderRadius: 5,
-          data: [
-            ["GRCD-AZ-AUT-PNP-R", 505992, 92],
-            ["Digitalexnonprod-Resource", 551695, 119],
-            ["GRCD-AZ-SUR-DP-RSG-01", 312679, 121],
-            ["grcd-az-sap-pnp-rsg-01", 78865, 136],
-            ["GRCD-AZ-SAP-PNP-RSG-01", 301336, 200],
-            ["GRCD-AZ-SUR-PNP-RSG-01", 41284, 213],
-            ["grcd-az-sur-pnp-rsg-01", 357114, 235],
-          ],
-          colors: [
-            "#63ace5", // Light Blue
-        "#98d26d", // Light Green
-        "#ffb366", // Light Orange
-        "#d18bff", // Light Purple
-        "#ff85a2", // Light Pink
-        "#ffdb5c", // Light Yellow
-        "#4fd1c5", // Light Teal
-          ],
+        lang: {
+          noData: "Please select atleast one Application",
         },
-      ],
-    });
-  }, []); // Empty dependency array ensures the effect runs once after initial render
+        noData: {
+          style: {
+            fontWeight: "bold",
+            fontSize: "15px",
+            color: "#303030",
+          },
+        },
+        legend: {
+          enabled: true,
+          // align: 'right',
+          // verticalAlign: 'top',
+          // layout: 'vertical',
+        },
+        series: [
+          {
+            type: "pie",
+            minPointSize: 100,
+            innerSize: "40%",
+            zMin: 0,
+            name: "networks",
+            borderRadius: 0,
+            colorByPoint: true,
+            data: props.data.data
+          }
+        ]
+      }
 
-  return <div id={"container-"+props.id} style={{ height: "400px" }} />;
+      Highcharts.chart("container-" + props.id, options);
+    }
+  }, [props.data]); // Empty dependency array ensures the effect runs once after initial render
+
+  return <div id={"container-" + props.id} style={{ height: "400px" }} />;
 };
 
 export default ChartComponent;
--- pages/page/FinOps/index.tsx
+++ pages/page/FinOps/index.tsx
@@ -6,7 +6,9 @@ import { useAppContext } from "../../Components/AppContext";
 import "rsuite/dist/rsuite.min.css";
 import PieChartComponent from "../../Components/Charts/PieChart";
 import StackChartComponent from "../../Components/Charts/StackChart";
-import LineChartComponent from "../../Components/Charts/LineChartBlendedCost";
+import LineChartComponent from "../../Components/Charts/LineChart";
+import AzureData from "./Azure.json";
+import AWSData from "./AWS.json";
 // import BubbleChartComponent from "./Charts/BubbleChart";
 
 const FinOps = () => {
@@ -17,68 +19,7 @@ const FinOps = () => {
   const { timeEnd, toggleTimeEnd } = useAppContext();
   const [cloud, setCloud] = useState("Azure");
   const [Id, setId] = useState();
-  const res: any = {
-    Graph: [
-      {
-        PieChart: {
-          title: "Resource Group consumption",
-          data: [
-            ["grcd-az-sap-dp-rsg-01", 1616],
-            ["digitalexdrprod-resource", 287],
-            ["grcd-az-sur-pp-rsg-01-asr", 146],
-            ["grcd-az-sur-dp-rsg-asr-01", 644],
-            ["grcd-az-sur-dp-rsg-01", 817],
-          ],
-        }
-      },
-      {
-        PieChart: {
-          title: "Cost Distribution at application level",
-          data: [
-            ["ECC-DR-DB+CI (On Demand)", 29778.70211209737],
-            ["DR-GRC Tool (SAP SURROUND)", 1251.6276209803618],
-            ["DR-Central Mfg Cockpit (SAP SURROUND)", 1202.8653640140838],
-            ["DR - TrueCopy Digital Sign (SAP SURROUND)", 2404.4580796147857],
-            ["DR-Surakhsha (SAP SURROUND)", 4585.552786759836],
-            ["ECC-DR-APP1", 648.4713706970215],
-            ["DR-Tonner Management DB (SAP SURROUND)", 5015.091064453125],
-            ["ECC-DR-APP2", 573.1545715332031],
-            ["DR-Tonner Management App (SAP SURROUND)", 0],
-          ],
-        }
-      },
-      {
-        LineChart: {
-          title: "Cost Distribution at application level",
-          data: [
-            ["ECC-DR-DB+CI (On Demand)", 29778.70211209737],
-            ["DR-GRC Tool (SAP SURROUND)", 1251.6276209803618],
-            ["DR-Central Mfg Cockpit (SAP SURROUND)", 1202.8653640140838],
-            ["DR - TrueCopy Digital Sign (SAP SURROUND)", 2404.4580796147857],
-            ["DR-Surakhsha (SAP SURROUND)", 4585.552786759836],
-            ["ECC-DR-APP1", 648.4713706970215],
-            ["DR-Tonner Management DB (SAP SURROUND)", 5015.091064453125],
-            ["ECC-DR-APP2", 573.1545715332031],
-            ["DR-Tonner Management App (SAP SURROUND)", 0],
-          ],
-        }
-      },
-      {
-        LineChart: {
-          title: "Cost Distribution at application level",
-          data: [
-            ["ECC-DR-DB+CI (On Demand)", 29778.70211209737],
-          ],
-        }
-      },
-    ],
-    Table: [],
-    Mertric: {
-      title: "Grasim Chemical Division - DR PROD",
-      value: 328460.59684130736,
-    },
-
-  };
+  const res: any = cloud == "Azure" ? AzureData : AWSData;
   const [value, setValue] = React.useState<any>([
     new Date(time),
     new Date(timeEnd),
@@ -232,34 +173,46 @@ const FinOps = () => {
         </div>
         <div className="card !w-full">
           <b>
-            <span>{res.Mertric.title} - </span>
+            <span>{res.Metric?.title} - </span>
           </b>
-          <span>{res.Mertric.value}</span>
+          <span>{res.Metric?.value}</span>
         </div>
         <div className="mt-4 h-auto flex flex-wrap gap-4">
-
           {res.Graph.map((e: any, i: any) => {
             if (e.PieChart) {
               return (
                 <div key={i} className="card">
-                  <PieChartComponent id={i} />
+                  <PieChartComponent id={i} data={e.PieChart} />
                 </div>
-              )
-            }
-            else if (e.LineChart) {
+              );
+            } else if (e.LineChart) {
               return (
-                <div key={i} className={e.LineChart.data.length>=2?"card !min-w-full":"card"}>
-                  <LineChartComponent Id={i} date={value} />
+                <div
+                  key={i}
+                  className={
+                    e.LineChart.series?.data?.length >= 20
+                      ? "card !min-w-full"
+                      : "card"
+                  }
+                >
+                  <LineChartComponent id={i} data={e.LineChart} />
                 </div>
-              )
+              );
+            } else if (e.HorizontalStackBarGraph) {
+              return (
+                <div
+                  key={i}
+                  className={
+                    e.HorizontalStackBarGraph.data.length >= 10
+                      ? "card !min-w-full"
+                      : "card"
+                  }
+                >
+                  <StackChartComponent id={i} date={value} />
+                </div>
+              );
             }
           })}
-          {/* <div className="card">
-            <PieChartComponent id="1" />
-          </div> */}
-          <div className="card">
-            <StackChartComponent />
-          </div>
         </div>
       </div>
     </div>
