49bc842803ced3ad027d6913d4cf5af603d9fb24
Siddharth Nalwaya <siddharth.nalwaya@revdau.com>
2023-12-04 10:46:43 +0530

    updated new server URLs

--- const.ts
+++ const.ts
@@ -1,5 +1,6 @@
-// export const baseURL = "http://localhost:8000/";
-export const baseURL = "http://10.47.98.164:9250/";
+export const baseURL = "http://localhost:8000/"; // localhost
+// export const baseURL = "http://10.47.98.164:9250/"; // dev server
+// export const baseURL = "http://10.47.128.23:5610/"; // prod server
 export const finopsBaseUrl = "http://95.217.191.79:8001"
 export const finopsServerBaseUrl = "http://10.47.98.164:9201"
 export const localHostBaseUrl = "http://localhost:9201"
